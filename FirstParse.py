bfbb36dbeb98a1309f40a4d04e10f7d32d99faf7
lipiner <ylipiner@gmail.com>
2017-11-05 16:50:06 +0200

    hi

---
+++ FirstParse.py
@@ -0,0 +1,6 @@
+from Parser import Parser
+
+
+class FirstParse (Parser):
+    def __init__(self, command):
+        Parser.__init__(self, command)
---
+++ SecondParse.py
@@ -0,0 +1,6 @@
+from Parser import Parser
+
+
+class SecondParse (Parser):
+    def __init__(self, command):
+        Parser.__init__(self, command)
