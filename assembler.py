f7a7149a454a5e504f4cd320b8f60037cc08adc3
shahar <shahar.aizenbud@gmail.com>
2017-11-06 21:02:37 +0200

    beginning of translator: creates the dictionaries

--- assembler.py
+++ assembler.py
@@ -1,3 +1,7 @@
+###########
+# imports #
+###########
+
 import sys
 import os
 from first_parse import FirstParse
@@ -8,7 +12,7 @@ from translator import Translator
 #############
 # constants #
 #############
-PATH_POS = 1
+PATH_POS = 1  # the arguments position for the file path
 ASM_SUFFIX_LENGTH = 3
 HACK_SUFFIX = "hack"
 ASM_SUFFIX = "asm"
@@ -18,35 +22,39 @@ LINE_BREAK = "\n"
 
 def assemble_file(file_name):
     """
-
-    :param file_name:
-    :return:
+    The function gets a file name from asm type and assembles it to binary code. It creates a hack file with he same
+    name in the same directory the contains the assembled binary code.
+    :param file_name: the name of the asm file to be assembled
     """
+    # opening the asm file
     with open(file_name) as input_file:
+        # figuring the output file name- replacing asm suffix to hack
         output_file_name = file_name.replace(ASM_SUFFIX, HACK_SUFFIX)
+        # opening the output file in writing mode
         with open(output_file_name, WRITING_MODE) as output_file:
             # first parse of the file
-            first_parser = FirstParse()
-            symbol_table = SymbolTable()
+            first_parser = FirstParse()  # creating a Parser for the first parse
+            symbol_table = SymbolTable()  # creating the symbol table of the file
             for line in input_file:
-                first_parser.set_command(line)
-                symbol_table.set_label(first_parser)
+                first_parser.set_command(line)  # setting the parser to the current line
+                symbol_table.set_label(first_parser)  # creating the symbols of the labels
+
             # second parse of the file
-            input_file.seek(0)
-            second_parser = SecondParse()
+            input_file.seek(0)  # setting the cursor to the beginning of the file
+            second_parser = SecondParse()  # creating a Parser for the second parse
             for line in input_file:
-                second_parser.set_command(line)
+                second_parser.set_command(line)  # setting the parser to the current line
                 second_parser.parse()
-                binary_line = Translator.translate(second_parser, symbol_table)
+                binary_line = Translator.translate(second_parser, symbol_table)  # translating to binary code
                 if binary_line:
-                    output_file.write(binary_line + LINE_BREAK)
+                    output_file.write(binary_line + LINE_BREAK)  # printing the binary code in the output file
 
 
 def assemble_directory(directory_name):
     """
-
-    :param directory_name:
-    :return:
+    The function gets a directory name and assembles all the asm files in it. The function creates matching hack
+    files in the directory with the assembled binary code
+    :param directory_name: the name of the given directory
     """
     files_list = os.listdir(directory_name)  # list of all the files' name in the subject directory
     for directory_file in files_list:
@@ -54,9 +62,12 @@ def assemble_directory(directory_name):
             asm_file = os.path.join(directory_name, directory_file)
             assemble_file(asm_file)
 
+# main part
 if __name__ == '__main__':
     path = sys.argv[PATH_POS]
+
+    # checks if the given path is a directory or a file
     if os.path.isdir(path):
-        assemble_directory(path)
+        assemble_directory(path)  # assembles all asm file in the directory
     else:
-        assemble_file(path)
+        assemble_file(path)  # assembles the given asm file
