1b51a6c460bb3899c3e82597dcc37893d68119ac
lipiner <ylipiner@gmail.com>
2017-11-06 17:09:22 +0200

    parser

--- SymbolTable.py
+++ SymbolTable.py
@@ -1,6 +1,13 @@
-
+from FirstParse import FirstParse
 
 class SymbolTable:
+    labels_dict = {}
+
+    @staticmethod
+    def set_label(parser):
+        name = parser.get_label_name
+        if name is not None:
+            SymbolTable.labels_dict[name] = line_number
 
     @staticmethod
     def find(name):
