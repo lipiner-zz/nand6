89c8b0a9ce714a2dee3b78e16592d765d060203e
lipiner <ylipiner@gmail.com>
2017-11-06 17:00:42 +0200

    parser

---
+++ main.py
@@ -0,0 +1,25 @@
+from FirstParse import FirstParse
+from SecondParse import SecondParse
+
+if __name__ == '__main__':
+    parser = FirstParse("(LOOP)")
+    print(parser.get_label_name())
+    parser = FirstParse("//(LOOP)")
+    print(parser.get_label_name())
+    print(parser.get_line_number())
+    parser = FirstParse("@wer")
+    print(parser.get_label_name())
+    parser = FirstParse("D = A")
+    print(parser.get_label_name())
+    parser = FirstParse("(LOOP) // sdad")
+    print(parser.get_label_name())
+    print(parser.get_line_number())
+    print(FirstParse.get_line_number())
+
+    parser = SecondParse("D = A")
+    print(parser.get_dest(), parser.get_comp(), parser.get_jump())
+    parser = SecondParse("D = M;JPE")
+    print(parser.get_dest(), parser.get_comp(), parser.get_jump())
+    parser = SecondParse("0;JEQ")
+    print(parser.get_dest(), parser.get_comp(), parser.get_jump())
+
